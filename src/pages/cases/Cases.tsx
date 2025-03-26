d690e65fac3f5d13a92529faf159e769877b97d1
bobbygrdn <bobbygrdn777@gmail.com>
2025-03-25 20:06:30 -0700

    add Cases component with styles and integration of MysteryCards

---
+++ src/pages/cases/Cases.tsx
@@ -0,0 +1,12 @@
+import MysteryCards from "../../components/mysteryCards/MysteryCards";
+import "./Cases.css";
+
+const Cases = () => {
+    return (
+        <article id="cases">
+            <MysteryCards />
+        </article>
+    );
+};
+
+export default Cases;
