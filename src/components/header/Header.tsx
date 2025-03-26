fc9bc1558ed880a1e7698fe5eb2e83ff02e501a1
bobbygrdn <bobbygrdn777@gmail.com>
2025-03-25 20:05:50 -0700

    add Header component with styles for the murder mystery theme

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,12 @@
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <header id="header">
+            <h1>Murder Mystery</h1>
+            <p>Select a mystery to solve</p>
+        </header>
+    )
+}
+
+export default Header
