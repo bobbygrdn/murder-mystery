1389f30e05d7eeda5ffd861c62ed2f71c38447ab
bobbygrdn <bobbygrdn777@gmail.com>
2025-03-25 20:06:25 -0700

    add Main component to structure the app with Header and Cases

---
+++ src/pages/Main.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/header/Header"
+import Cases from "./cases/Cases"
+
+const Main = () => {
+    return (
+        <main id="app">
+            <Header />
+            <Cases />
+        </main>
+    )
+}
+
+export default Main
